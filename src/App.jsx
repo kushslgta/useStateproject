3fb91662ac460a9d522d604bc099781bbe966d8f
kushal <kushalagrawal778@gmail.com>
2021-12-05 09:55:25 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+ import React, { useState }  from "react";
+ const App =()=>{
+    const purple ="#8e44ad";
+
+    const[bg,setBg]=useState(purple);
+    const[name,setName]=useState("click me");
+
+ const bgChange=()=>{
+    //  console.log("clicked");
+    let newBg="#34495e";
+    setBg(newBg);
+    setName('Ooch!!');
+ };
+ const bgBack=()=>{
+    setBg(purple);
+    setName('Aiyooo');
+
+ };
+     return(
+         <>
+         <div style={{backgroundColor:bg}}>
+             <button onMouseEnter={bgChange} onMouseLeave={bgBack}>{name}</button>
+         </div>
+         </>
+     );
+ }
+ export default App;
